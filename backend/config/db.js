c8a3a501da35103250edb041a49f8c84b4a11c74
Om Pathak <ompathakin194@gmail.com>
2025-07-24 23:24:32 +0530

    added already exists command for database removed font import from index.css

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,4 +3,20 @@ import dotenv from "dotenv";
 dotenv.config();
 const pool = new Pool();
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS user_todo (
+        id SERIAL PRIMARY KEY,
+        description TEXT NOT NULL
+      );
+    `);
+    console.log("✅ Table user_todo is ready");
+  } catch (err) {
+    console.error("❌ Failed to create table", err);
+  }
+};
+
+initDB();
+
 export default pool;
